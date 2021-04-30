f48536219e654815fd765fb6cc2df285dd42a42e
jbuoncri <jbuoncri@cisco.com>
2021-04-30 14:51:45 -0700

    added semantic and materialize modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
 
-function App() {
+// import 'materialize-css/dist/css/materialize.min.css'
+// import M from 'materialize-css/dist/js/materialize.min.js'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+
+const App = () => {
+  // useEffect(() => {
+  //   // Initialize Materialize CSS
+  //   M.AutoInit()
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="ui container"
+    >
+      <h1
+        style={{
+          fontSize: 24,
+          fontWeight: 700,
+          fontFamily: 'Roboto'
+        }}
+      >
+        Welcome To Trellis
+      </h1>
+      <Button 
+        color="primary color"
+        style={{
+          borderRadius: 4,
+          fontFamily: 'Roboto',
+          width: 140,
+          height: 45
+        }}
+      >
+        Sign Up
+      </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
